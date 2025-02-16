f49cf27682803d7048367d529b8843d31e17cf19
Mylliah <mezhoud.my@gmail.com>
2025-02-16 15:36:09 +0100

    Add 4-puts.c file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Uses puts to print a text
+ * Return: 0 for success
+ */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
 
