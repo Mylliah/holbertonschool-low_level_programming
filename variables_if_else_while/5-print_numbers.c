6d83e0bd77396d41af7a749a560b7d4545e6d432
Mylliah <mezhoud.my@gmail.com>
2025-02-17 11:03:12 +0100

    Add 5-print_numbers.c file

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 0 ; alpha < 10 ; alpha++)
+	{
+		printf("%d", alpha);
+	}
+	printf("\n");
+	return (0);
+}
