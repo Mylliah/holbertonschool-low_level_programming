8322eafb7134a9673632c111861987ee682ed94f
Mylliah <mezhoud.my@gmail.com>
2025-02-17 14:26:39 +0100

    Add 9-print_comb.c file

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0 ; num < 10 ; num++)
+	{
+		putchar('0' + num);
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
