c21e83ac123aff7ecfff4b6508c86041b4cfd5a9
Mylliah <mezhoud.my@gmail.com>
2025-02-21 19:07:20 +0100

    Add 10-add.c file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point to addition two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
