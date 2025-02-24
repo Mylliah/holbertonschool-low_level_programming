41fe0d551d8d8817517b8738e663e2f190a32dcc
Mylliah <mezhoud.my@gmail.com>
2025-02-24 11:13:50 +0100

    Add 0-isupper.c file

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point to check for lowercase character
+ * @c: character to check
+ *
+ * Return: 1 if lowercase character, else 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return(0);
+}
