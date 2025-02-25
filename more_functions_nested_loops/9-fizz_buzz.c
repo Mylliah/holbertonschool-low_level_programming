1e65b1e7a99a5b4ce849a3c489c09a37ee5a2a77
Mylliah <mezhoud.my@gmail.com>
2025-02-25 21:58:49 +0100

    Add 9-fizz_buzz.c file

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * _putchar - function that prints
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1 ; num <= 100 ; num++)
+	{
+		if (num % 3 == 0)
+		{	printf("Fizz");
+			putchar(' ');
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d", num);
+			putchar(' ');
+		}
+	}
+	return (0);
+}
