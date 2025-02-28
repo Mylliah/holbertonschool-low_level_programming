927db7a7507e06db61eb3f16bcfdbea7929bc99d
Mylliah <mezhoud.my@gmail.com>
2025-02-28 10:09:35 +0100

    Add 6-puts.c file

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints other caracter of a string
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int string = 0;
+
+	while (str[string] != '\0')
+	{
+		_putchar(str[string]);
+		string += 2;
+	}
+	_putchar('\n');
+}
