b5a3c7b97ff499d3c7e6d4a3d8dbeb4dedcc351b
Mylliah <mezhoud.my@gmail.com>
2025-03-02 19:11:59 +0100

    Add 100-atoi.c file

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,30 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - function that covert a string to an integer
  * @s: string
  *
- * Return: integer 
+ * Return: integer
  */
 
 int _atoi(char *s)
 {
 	int i = 0;
-	int sign = 1; 
+	int sign = 1;
 	int result = 0;
-	int num;
+	int end = 0;
 
-	while (s[i] == ' ')
-		i++;
-	
-
-	while (s[i] == '-' || s[i] == '+')
+	for (i = 0 ; s[i] != '\0' ; i++)
 	{
 		if (s[i] == '-')
-			sign = -1;
-		i++;
-	}
+			sign = -sign;
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		num = s[i] - '0';
-		
-		if (result > (INT_MAX - num) / 10)
+		if (s[i] >= '0' && s[i] <= '9' && end == 0)
 		{
-			if (sign == 1)
-				return INT_MAX;
-			else	
-				return INT_MIN;
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+				end = 1;
+			result = result * 10 + (s[i] - '0');
 		}
-		result = result * 10 + num;
-		i++;
 	}
 	result = result * sign;
 	return (result);
