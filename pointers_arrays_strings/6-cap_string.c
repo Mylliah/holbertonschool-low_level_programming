832b918c4e7d0ff580fa5aa38edc0cb513f40759
Mylliah <mezhoud.my@gmail.com>
2025-03-02 13:24:55 +0100

    Add 6-cap_string file

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @str: string
+ *
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0; /* index of str */
+	int sep_i = 0; /* index of separator's array */
+	char separator[] = " \t\n,;.!?\"(){}";
+	int uppercase = 1; /* boolean to indicate whether we must capitalize */	
+
+	while (str[i] != '\0')
+	{
+		if (uppercase && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+		uppercase = 0;
+
+		for (sep_i = 0 ; separator[sep_i] != '\0' ; sep_i++)
+		{	
+			if (str[i] == separator[sep_i])
+			{
+				uppercase = 1;
+				break;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
