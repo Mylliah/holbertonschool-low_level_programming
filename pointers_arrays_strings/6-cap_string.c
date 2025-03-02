d044c295783514b5f40d57c1dece442b44a8568f
Mylliah <mezhoud.my@gmail.com>
2025-03-02 13:32:54 +0100

    Add 6-cap_string file

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 	int i = 0; /* index of str */
 	int sep_i = 0; /* index of separator's array */
 	char separator[] = " \t\n,;.!?\"(){}";
-	int uppercase = 1; /* boolean to indicate whether we must capitalize */	
+	int uppercase = 1; /* boolean to indicate whether we must capitalize */
 
 	while (str[i] != '\0')
 	{
@@ -21,7 +21,7 @@ char *cap_string(char *str)
 		uppercase = 0;
 
 		for (sep_i = 0 ; separator[sep_i] != '\0' ; sep_i++)
-		{	
+		{
 			if (str[i] == separator[sep_i])
 			{
 				uppercase = 1;
