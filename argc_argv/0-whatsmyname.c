526640562be5bc1530641cf980108e3cfe99b793
Mylliah <mezhoud.my@gmail.com>
2025-03-05 17:41:00 +0100

    Add 0-whatsmyname.c file

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - that prints the name's program
+ * @argc: arguments number
+ * @argv: arguments array
+ *
+ * Return: 0
+ */
+
+
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
