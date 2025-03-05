bf12bbabed82f8efa1ff02e1ce646c1752e41d61
Mylliah <mezhoud.my@gmail.com>
2025-03-05 17:56:01 +0100

    Add 1-args.c file

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - that prints the name's program
+ * @argc: arguments number
+ * @argv: arguments array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv __attribute__((__unused__)))
+{
+		printf("%d\n", argc - 1);
+	return (0);
+}
