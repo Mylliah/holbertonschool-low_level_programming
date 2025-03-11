c2c54952f47c5de22aeb80b3e7ce25de3cd7a7c7
Mylliah <mezhoud.my@gmail.com>
2025-03-11 12:50:22 +0100

    Add 0-create_array.c file

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, initializes with a specific char
+ * @size: size of the array
+ * @c: containt of array
+ *
+ * Return: pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(size * sizeof(char));
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < size ; i++)
+		array[i] = c;
+
+	return (array);
+}
