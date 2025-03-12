f972eb1c5f2d9ef33b1b6ed7bb527ed7e9da40b3
Mylliah <mezhoud.my@gmail.com>
2025-03-12 13:51:47 +0100

    Add 0-malloc_checked.c file

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: positive integer
+ *
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *checked;
+
+	checked = malloc(b);
+
+	if (checked == NULL)
+		exit(98);
+
+	return (checked);
+}	
