0ca91c1ba46c6464c1ee972d8d6992e359cfeeac
Mylliah <mezhoud.my@gmail.com>
2025-03-13 21:34:19 +0100

    Add dog.h file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,13 +7,17 @@
  * @age: dog age
  * @owner: dog owner
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef to struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
