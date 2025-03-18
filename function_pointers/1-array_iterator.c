19029ee374de63b272b63ba8c42a8296852b229f
Mylliah <mezhoud.my@gmail.com>
2025-03-18 09:01:19 +0100

    Add 1-array_iterator.c file

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - function that dynamically executes the function pointed by action
+ * @array: pointer to the array of integers
+ * @size: size of the array
+ * @action: pointer to the function to be executed
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	for (i = 0 ; i < size ; i++)
+		action(array[i]);
+}
