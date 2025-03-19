ce4d300d96294c9edba921bb4f27419f9d423112
Mylliah <mezhoud.my@gmail.com>
2025-03-19 21:47:49 +0100

    Add 1-print_numbers.c file

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - function that prints numbers with separator between
+ * @separator: string to separate numbers
+ * @n: number of integers passed to the function
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int numbers = 0;
+	va_start(args, n);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		numbers = va_arg(args, int);
+		printf("%d", numbers);
+		if (i < n - 1)
+		{
+			if (separator == NULL)
+				continue;
+			else
+				printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
