84586208090cb6f50cdff464ab5abbaafe4258a3
Mylliah <mezhoud.my@gmail.com>
2025-04-02 18:12:43 +0200

    Correction 0-print_dlistint.c file

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,6 @@
 #include "lists.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to print
- */
-void print_number(int n)
-{
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
-}
-
-
-/**
  * print_dlistint - prints all elements of a doubly linked list
  * @h: pointer to the head of the list
  *
@@ -22,10 +10,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
 
+	if (h == NULL)
+		return (0);
+
 	while (h)
 	{
-		print_number(h->n);
-		_putchar ('\n');
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
